2681184b1c9481298accef7c339ca33e7102e532
imvickykumar999 <imvickykumar999@gmail.com>
2022-02-25 23:14:24 +0530

    import By
    https://stackoverflow.com/a/69875984/11493297

--- autologin/cracker.py
+++ autologin/cracker.py
@@ -1,33 +1,39 @@
 
+# https://stackoverflow.com/a/69875984/11493297
+from selenium.webdriver.common.by import By
+
 from selenium import webdriver
 import time, random
 
-web = webdriver.Chrome()
-Email = "login_bot_"
+Email = "bot_"
 passw = str(random.randint(0, 50))
+
 print(f'''
 >>>> passw : {passw}
 ''')
+
 add_Email = []
-counter = 0
+chance = counter = 0
+
+web = webdriver.Chrome()
 
 def fun_signup(Email):
     web.get('http://127.0.0.1:5000/signup/')
 
-    first = web.find_element_by_xpath('//*[@id="username"]')
+    first = web.find_element(By.XPATH, '//*[@id="username"]')
     first.send_keys(Email)
 
-    last = web.find_element_by_xpath('//*[@id="password"]')
+    last = web.find_element(By.XPATH, '//*[@id="password"]')
     last.send_keys(passw)
 
-    bio = web.find_element_by_xpath('//*[@id="bio"]')
+    bio = web.find_element(By.XPATH, '//*[@id="bio"]')
     bio.send_keys(passw, ' has been cracked.')
 
-    Submit = web.find_element_by_xpath('//*[@id="signup"]/input[3]')
+    Submit = web.find_element(By.XPATH, '//*[@id="signup"]/input[3]')
     Submit.click()
 
     try:
-        check = web.find_element_by_css_selector('.flashes').text
+        check = web.find_element(By.CSS_SELECTOR, '.flashes').text
         if check.split()[0] == 'Username':
 
             print(f'''
@@ -52,37 +58,53 @@ def fun_signup(Email):
 def fun_login(passw):
     web.get('http://127.0.0.1:5000/login/')
 
-    first = web.find_element_by_xpath('//*[@id="username"]')
+    first = web.find_element(By.XPATH, '//*[@id="username"]')
     first.send_keys(Email)
 
-    last = web.find_element_by_xpath('//*[@id="password"]')
+    last = web.find_element(By.XPATH, '//*[@id="password"]')
     last.send_keys(passw)
 
-    Submit = web.find_element_by_xpath('//*[@id="submit"]')
+    Submit = web.find_element(By.XPATH, '//*[@id="submit"]')
     Submit.click()
 
     try:
-        check = web.find_element_by_css_selector('.flashes')
+        check = web.find_element(By.CSS_SELECTOR, '.flashes')
         if check.text == 'Invalid username or password.':
             
             print(f'''
             >>>> {check.text}
             ''')
 
-            passw = str(int(passw) + 1)
-            fun_login(passw)
+            guessed_passw = str(int(passw) + 1)
+
+            # GAME...
+            # guessed_passw = input('Enter guessed Password : ')
+            # if int(guessed_passw) > int(passw):
+
+            #     print('Password is Greater than Guessed...')
+            #     chance += 1
+
+            # elif int(guessed_passw) < int(passw):
+            #     print('Password is Lesser than Guessed...')
+            #     chance += 1
+
+            # if int(guessed_passw) == int(passw):
+            #     print('Guessed Password is Correct...')
+            #     print('>>>>>>> ', chance)
+
+            fun_login(guessed_passw)
     except:
         pass
 
     try:
-        check = web.find_element_by_css_selector('.welcome').text
+        check = web.find_element(By.CSS_SELECTOR, '.welcome').text
         if check == 'Welcome':
 
             input('''
             >>> Press Enter to Logout...
             ''')
 
-            logout = web.find_element_by_xpath('/html/body/p/a')
+            logout = web.find_element(By.XPATH, '/html/body/p/a')
             logout.click()
     except:
         pass
@@ -94,5 +116,6 @@ Email = fun_signup(Email)[0]
 
 # to automate login
 passw = "0"
+# Email = "bot_"
 time.sleep(2)
 fun_login(passw)
